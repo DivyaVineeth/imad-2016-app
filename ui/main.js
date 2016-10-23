d89b229d92595d7df6ed38fb0c59a21ab4afe82c
DivyaVineeth <divyar.krishna@gmail.com>
2016-10-23 09:51:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,10 @@ console.log('Loaded!');
 //change the text of the main-text
 var element=document.getElementById('main-text');
 element.innerHTML="New girl";  
+
+//move image
+var img=document.getElemebtById('img');
+img.onClick=function(){
+  img.style.marginleft='100px';  
+    
+};
